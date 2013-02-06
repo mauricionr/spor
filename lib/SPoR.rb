945dc2f0eef70f0f370cd7124ed228a149fa6956
mgoldbach <mgoldbach@agile-is.de>
2013-02-06 10:05:23 +0100

    refactoring

--- lib/SPoR.rb
+++ lib/SPoR.rb
@@ -4,22 +4,21 @@ require_relative "SPoR/sp_web"
 require_relative "SPoR/sp_search"
 
 module SPoR
-  def self.connect_to_web(sp_apptoken, sp_host_url)
+  extend self
+
+  def connect_to_web(sp_apptoken, sp_host_url)
     if sp_host_url.nil?
       raise "SharePoint Host Url is empty"
     end
-    connectionargs = {:sp_apptoken => sp_apptoken, :sp_host_url => sp_host_url}
-    sp_connection = SPConnection.new connectionargs
-
-    return SPWeb.new sp_connection
+    sp_connection = SPConnection.new({:sp_apptoken => sp_apptoken, :sp_host_url => sp_host_url})
+    SPWeb.new sp_connection
   end
 
-  def self.connect_to_search(sp_apptoken, sp_host_url)
+  def connect_to_search(sp_apptoken, sp_host_url)
     if sp_apptoken.nil? or sp_host_url.nil?
       raise "parameter is empty"
     end
-    connectionargs = {:sp_apptoken => sp_apptoken, :sp_host_url => sp_host_url}
-    sp_connection = SPConnection.new connectionargs
+    sp_connection = SPConnection.new({:sp_apptoken => sp_apptoken, :sp_host_url => sp_host_url})
 
   end
 end
